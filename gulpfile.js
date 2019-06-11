7c7305c4a45d112eea1f9a74f4f900ecaa5e56f3
mac <mac@macs-MacBook-Pro.local>
2019-06-11 10:51:55 +0200

    add build command

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
- var autoprefixer = require('gulp-autoprefixer');
+var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
-watch = require('gulp-watch');
+var watch = require('gulp-watch');
+var build = require('gulp-build');
 
 
 gulp.task('default', () => {
-  return watch('src/css/**/*.css' , function(){
-    gulp.src('src/css/**/*.css')
-    .pipe(minifyCSS())
-     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
-    .pipe(concat('style.min.css'))
-    .pipe(gulp.dest('dist/css'))
-  })
-})
+    return watch('src/css/**/*.css', function () {
+        gulp.src('src/css/**/*.css')
+            .pipe(minifyCSS())
+            .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+            .pipe(concat('style.min.css'))
+            .pipe(gulp.dest('dist/css'))
+    })
+});
+
+gulp.task('build', () => {
+    return gulp.src('src/css/**/*.css')
+        .pipe(minifyCSS())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('dist/css'));
+});
 
